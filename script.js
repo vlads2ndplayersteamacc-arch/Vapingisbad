6cb40f582b81741b434ccac5227541d5e463b21b
vlads2ndplayersteamacc-arch <vlads2ndplayersteamacc@gmail.com>
2026-03-24 09:57:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function showFact() {
+    const facts = [
+        "Nicotine can change brain development in teens.",
+        "Some vapes contain heavy metals.",
+        "Vaping can lead to long-term addiction.",
+        "It affects focus, mood, and memory."
+    ];
 
+    const random = facts[Math.floor(Math.random() * facts.length)];
+    document.getElementById("fact").innerText = random;
+}
+
+function quiz(answer) {
+    if (answer === "yes") {
+        document.getElementById("quizResult").innerText =
+            "You may already be developing dependence.";
+    } else {
+        document.getElementById("quizResult").innerText =
+            "Stay aware. It can start anytime.";
+    }
+}
+
+function scrollToSection() {
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: "smooth"
+    });
+}
